7ad6f46a1ca10489b4187d63de161c770819b77d
lKoi <iago_neosky@hotmail.com>
2016-12-17 13:34:12 +0100

    sprint2

---
+++ javascript/menu-sidenav.js
@@ -0,0 +1,30 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function selectTeam() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
